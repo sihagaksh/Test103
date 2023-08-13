e7d47819feed1a3695f6dc1393d0ffe5c23318c8
Aksh Sihag <142142152+sihagaksh@users.noreply.github.com>
2023-08-13 18:50:25 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,3 +2,4 @@
 print('Hello World')
 #This is me checking that this file goes into the terminal or not.
 #This is me checking once again that this goes into the browser or not:x
+#let us check if the comment goes in the terminal
